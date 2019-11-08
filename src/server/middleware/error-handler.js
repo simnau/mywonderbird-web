e1230d9a815bfeb4a3f8ec0aae2e5a3b5cf0bc63
Simonas Naulickas <simonas.naulickas@gmail.com>
2019-11-08 07:27:09 +0200

    Update error handler to use error status, rather than error code as HTTP status

--- src/server/middleware/error-handler.js
+++ src/server/middleware/error-handler.js
@@ -3,7 +3,7 @@ const logger = require('../util/logger');
 module.exports = (err, req, res, next) => {
   logger.error(err.message);
 
-  res.status(err.code || 500).send({
+  res.status(err.status || 500).send({
     error: err.message || err,
   });
 };
