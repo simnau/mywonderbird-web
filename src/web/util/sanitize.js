324769f5da6097e5cfdcad9b08b6f3be2fb18813
Simonas Naulickas <simonas.naulickas@gmail.com>
2019-12-30 13:56:19 +0200

    Fix sanitize util from removing false values

--- src/web/util/sanitize.js
+++ src/web/util/sanitize.js
@@ -2,7 +2,7 @@ export function sanitizeObject(object, fieldsToSanitize, initialValue = {}) {
   return fieldsToSanitize.reduce((acc, field) => {
     const fieldValue = object[field];
 
-    if (!fieldValue && fieldValue !== 0) {
+    if (!fieldValue && fieldValue !== 0 && fieldValue !== false) {
       return acc;
     }
 
