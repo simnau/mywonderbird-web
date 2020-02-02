ee8e787307fe5f0f898156ef29058d98d9dc3c52
Simonas Naulickas <simonas.naulickas@gmail.com>
2020-02-02 13:54:38 +0200

    Add terms constants

---
+++ src/server/constants/terms.js
@@ -0,0 +1,19 @@
+const TERMS_OF_SERVICE_TYPE = 'terms-of-service';
+const PRIVACY_POLICY_TYPE = 'privacy-policy';
+
+const TYPES = [
+  {
+    label: 'Terms Of Service',
+    value: TERMS_OF_SERVICE_TYPE,
+  },
+  {
+    label: 'Privacy Policy',
+    value: PRIVACY_POLICY_TYPE,
+  },
+];
+
+module.exports = {
+  TERMS_OF_SERVICE_TYPE,
+  PRIVACY_POLICY_TYPE,
+  TYPES,
+};
