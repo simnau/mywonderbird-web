56bcf37281d30a109d9313fbcbb32317b4b390f4
Simonas Naulickas <simonas.naulickas@gmail.com>
2020-10-16 20:46:36 +0300

    Fix migration not working

--- src/server/orm/migrations/00010-change-profile-id-type.js
+++ src/server/orm/migrations/00010-change-profile-id-type.js
@@ -3,23 +3,13 @@ const { Profile } = require('../models/profile');
 // This probably should've been a one-of task
 module.exports = {
   async up(queryInterface, Sequelize) {
-    // await queryInterface.addColumn('profiles', 'providerId', {
-    //   type: Sequelize.STRING,
-    //   defaultValue: Sequelize.UUIDV4,
-    //   unique: true,
-    // });
-
-    // const profiles = await Profile.findAll();
-
-    // return Promise.all(
-    //   profiles.map(profile => {
-    //     return profile.update({
-    //       providerId: profile.id,
-    //     });
-    //   }),
-    // );
+    return queryInterface.addColumn('profiles', 'providerId', {
+      type: Sequelize.STRING,
+      defaultValue: Sequelize.UUIDV4,
+      unique: true,
+    });
   },
   down(queryInterface, Sequelize) {
-    // return queryInterface.removeColumn('profiles', 'providerId');
+    return queryInterface.removeColumn('profiles', 'providerId');
   },
 };
