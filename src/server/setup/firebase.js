877e641cffc9b89837b94f3929ce8d133004d2ae
Simonas Naulickas <simonas.naulickas@gmail.com>
2021-04-13 19:01:56 +0300

    Update circleci config

--- src/server/setup/firebase.js
+++ src/server/setup/firebase.js
@@ -1,7 +1,10 @@
 const admin = require('firebase-admin');
+const base64 = require('base-64');
 const config = require('config');
 
-const serviceKey = config.get('google.serviceKey');
+const serviceKeyBase64 = config.get('google.serviceKey');
+
+const serviceKey = base64.decode(serviceKeyBase64);
 
 admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(serviceKey)),
